74f476c894be5a60b306090bb6f8b839195ba720
Estifanos <estifanosbereket297@gmail.com>
2025-03-19 16:39:44 +0300

    feat : initial supabase config

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/intro.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: "https://iazgcqadmrjhszpeqxpj.supabase.co",
+    anonKey:
+        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlhemdjcWFkbXJqaHN6cGVxeHBqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIzODMxNDksImV4cCI6MjA1Nzk1OTE0OX0.v70ChJdX7BiAjvW3DmeZ1ekZ9gKGQ5zNxgbaKfsCC9c",
+  );
+
+  runApp(MyApp());
 }
+
+// final supabase = Supabase.instance.client;
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -17,7 +27,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Coffee Masters',
+      title: 'Nova Todo',
       theme: ThemeData(primarySwatch: Colors.brown),
       home: const IntorPage(),
       debugShowCheckedModeBanner: false,
---
+++ lib/ui/inputs/dateselector.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart'; // Make sure to add intl package in your pubspec.yaml
+
+class DateSelector extends StatelessWidget {
+  final String hintText;
+  final TextEditingController controller;
+  final IconData icon;
+  final DateTime firstDate;
+  final DateTime lastDate;
+  final DateTime? initialDate;
+  final DateFormat dateFormat;
+
+  // Removed "const" from the constructor to allow runtime DateFormat creation.
+  DateSelector({
+    Key? key,
+    required this.hintText,
+    required this.controller,
+    required this.icon,
+    required this.firstDate,
+    required this.lastDate,
+    this.initialDate,
+    DateFormat? dateFormat,
+  }) : dateFormat = dateFormat ?? DateFormat('dd-MM-yyyy'),
+       super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      readOnly: true, // Prevents keyboard from showing
+      controller: controller,
+      style: const TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        prefixIcon: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          decoration: const BoxDecoration(
+            border: Border(
+              right: BorderSide(color: Colors.white54, width: 1.0),
+            ),
+          ),
+          child: Icon(icon, color: Colors.white70),
+        ),
+        prefixIconConstraints: const BoxConstraints(minWidth: 50),
+        hintText: hintText,
+        filled: true,
+        fillColor: Colors.black87.withOpacity(.3),
+        hintStyle: const TextStyle(
+          fontSize: 11,
+          fontWeight: FontWeight.w200,
+          color: Colors.white60,
+        ),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: const BorderSide(width: 2, color: Colors.green),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.green.withOpacity(.3),
+            width: 1.0,
+          ),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.black38.withOpacity(.3),
+            width: 1.0,
+          ),
+        ),
+      ),
+      onTap: () async {
+        DateTime initDate = initialDate ?? DateTime.now();
+        DateTime? pickedDate = await showDatePicker(
+          context: context,
+          initialDate: initDate,
+          firstDate: firstDate,
+          lastDate: lastDate,
+          builder: (context, child) {
+            return Theme(
+              data: Theme.of(context).copyWith(
+                colorScheme: ColorScheme.dark(
+                  primary: Colors.green, // header background color
+                  onPrimary: Colors.white, // header text color
+                  surface: Colors.black87, // background color
+                  onSurface: Colors.white70, // body text color
+                ),
+                dialogBackgroundColor: Colors.black,
+              ),
+              child: child ?? const SizedBox(),
+            );
+          },
+        );
+        if (pickedDate != null) {
+          controller.text = dateFormat.format(pickedDate);
+        }
+      },
+    );
+  }
+}
